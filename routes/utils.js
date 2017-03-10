6b96659f2f840794d154fbf94eb61aa75eb5a980
poanchen <chen.baron@hotmail.com>
2017-03-09 20:02:54 -0800

    debugging

--- routes/utils.js
+++ routes/utils.js
@@ -454,7 +454,10 @@ function findNextMove(data) {
       // I think we are hungry and we should go ahead and eat some food
       // but before we go ahead, let's see if it is safe to do so
       for (var i = 0; i < safeMoves.size; i++) {
-        console.log("data.shortestPath" + data.get('shortestPath'))
+        console.log("data.shortestPath " + data.get('shortestPath'))
+        console.log(safeMoves.get(i)[0] == data.getIn(['shortestPath', 1])[0])
+        console.log(safeMoves.get(i)[1] == data.getIn(['shortestPath', 1])[1])
+        console.log('sdfds')
         if (safeMoves.get(i)[0] == data.getIn(['shortestPath', 1])[0] &&
           safeMoves.get(i)[1] == data.getIn(['shortestPath', 1])[1]) {
           // I think it is safe to eat
